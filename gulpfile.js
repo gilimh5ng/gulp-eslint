b52a0a9eb8f46d6cf81026c023e4d17d12946682
Gilim Hong <gilim.h5ng@gmail.com>
2016-12-08 22:04:14 +0900

    lint task 추가
    lint rules, options 등 설정하여 lint 테스트 후에 log파일로 기록

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,97 @@
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var gulpopen = require('gulp-open');
+'use strict';
 
+let fs = require('fs');
+let gulp = require('gulp');
+let eslint = require('gulp-eslint');
+let gulpopen = require('gulp-open');
+let results = [];
 
-var src = './src/*.js';
+gulp.task('lint', () => {
+    let src = './src/*.js'; // 검사 파일
+    let rules = { // eslint 규칙
+        'camelcase': 1,
+        'comma-dangle': 1,
+        'quotes': [1, 'single'],
+        'semi': 2,
+        'indent': [1, 4]
+    };
 
+    let parserOptions = {
+        'ecmaVersion': 6
+    };
 
-gulp.task('lint', function() {
     return gulp.src(src)
-        .pipe(eslint({
-            'rules': {
-                'camelcase': 1,
-                'comma-dangle': 1,
-                'quotes': [1, 'single'],
-                'semi': 2,
-                'indent': [1, 4]
-            }
-        }))
+        .pipe(eslint({ rules, parserOptions }))
         .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+        .pipe(eslint.result((r) => {
+            results.push(r);
+        }));
 });
 
-gulp.task('default', ['lint'], function() {
-    console.log('Finish!');
+gulp.task('default', ['lint'], () => {
+    writeLog();
 });
+
+
+let writeLog = () => {
+    let r = results;
+    let log = '';
+    let logText = '';
+    let totalProblemCount = 0;
+    let totalWarningCount = 0;
+    let totalErrorCount = 0;
+
+
+    for(let i=0; i<r.length; i++) {
+        let filePath = r[i].filePath;
+        let warningCount = r[i].warningCount;
+        let errorCount = r[i].errorCount;
+        let messages = r[i].messages;
+
+        totalProblemCount += warningCount + errorCount;
+        totalWarningCount += warningCount;
+        totalErrorCount   += errorCount;
+
+        logText += i > 0 ? '\n\n\n' : '';
+        logText +=  filePath + '\n\n';
+        logText += '* Warnig Count : ' + warningCount + '\n';
+        logText += '* Error Count : ' + errorCount + '\n\n';
+
+        for(let j=0; j<messages.length; j++) {
+            let msg = messages[j];
+            logText += (i+1) + '-' + (j+1) + '. ';
+            logText += msg.severity === 1 ? 'warning' : 'error';
+            logText += ' ' + msg.line + ':' + msg.column + ' ==> ' + msg.message + '\n';
+        }
+    }
+
+    log += '*** ' + totalProblemCount + ' problem';
+    log += totalProblemCount > 1 ? 's' : '';
+    log += ' (' + totalErrorCount + ' error';
+    log += totalErrorCount > 1 ? 's' : '';
+    log += ', ' + totalWarningCount + ' warning';
+    log += totalWarningCount > 1 ? 's' : '';
+    log += ') ***\n\n\n';
+    log += logText;
+
+    let date = new Date();
+    let yyyy = date.getFullYear().toString();
+    let MM = _addZero((date.getMonth()+1).toString());
+    let dd = _addZero(date.getDate().toString());
+    let hh = _addZero(date.getHours().toString());
+    let mm = _addZero(date.getMinutes().toString());
+    let ss = _addZero(date.getSeconds().toString());
+    let SSS = date.getMilliseconds().toString();
+
+    let fileName = 'log_' + yyyy + MM + dd + "_" + hh + mm + ss + "_" + SSS + ".log";
+
+    fs.writeFileSync("./logs/" + fileName, log , 'utf-8');
+};
+
+
+let _addZero = i => {
+    if(Number(i) < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
